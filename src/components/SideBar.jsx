7603f0c3b4da572cf7e1480de4730b8d30106411
KR-ISCOD3 <krisjenxz@gmail.com>
2025-03-10 20:37:28 +0700

    update-new

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,63 +1,74 @@
 import React from "react";
-import { FaBook, FaRegWindowRestore, FaUserCog, FaUserFriends } from "react-icons/fa";
+import { FaBookOpen, FaUserFriends, FaUserCog, FaRegWindowRestore } from "react-icons/fa";
 import { MdDateRange } from "react-icons/md";
 import { RiHomeSmile2Fill } from "react-icons/ri";
 import { NavLink } from "react-router-dom";
-import { FaBookOpen } from "react-icons/fa";
+import { IoLogOut } from "react-icons/io5";
 
 function SideBar() {
   return (
     <aside className="col-2">
-      <div className="col-2 position-fixed p-3 font-poppins bg-blue-700" style={{height:"100vh"}}>
-        <header className="d-flex align-items-center justify-content-start text-light border-secondary-subtle border-bottom pb-1">
-          <div style={{width:60,height:60}} className="rounded-circle bg-light overflow-hidden me-2 mb-2">
-            <img src="" alt="" className="w-100 h-100 object-fit-cover"/>
+      <div className="col-2 position-fixed p-3 font-poppins bg-blue-700 d-flex flex-column" style={{ height: "100vh" }}>
+        {/* Header */}
+        <header className="d-flex align-items-center text-light border-bottom pb-3">
+          <div style={{ width: 60, height: 60 }} className="rounded-circle bg-light overflow-hidden me-2">
+            <img src="" alt="" className="w-100 h-100 object-fit-cover" />
           </div>
           <div>
-            <h5 className="m-0 ">FE-Manager</h5>
-            <p style={{fontSize:"12px"}} className="m-0"></p>
+            <h5 className="m-0">FE-Manager</h5>
+            <p style={{ fontSize: "12px" }} className="m-0"></p>
           </div>
         </header>
-        <menu className="p-0">
+
+        {/* Menu */}
+        <menu className="p-0 flex-grow-1">
           <ul className="list-unstyled">
-              <NavLink to="/" className="d-flex align-items-center hover-li text-light nav-link rounded-2 my-2">
-                <li className="py-2 px-3">
-                  <FaRegWindowRestore className="me-2 fs-5"/>
-                  Dashboard
-                </li>
-              </NavLink>
-              <NavLink to="/student" className="d-flex align-items-center hover-li text-light nav-link rounded-2 my-2">
-                <li className="py-2 px-3">
-                  <FaUserFriends className="me-2 fs-5"/>
-                  Students
-                </li>
-              </NavLink>
-              <NavLink to="/teacher" className="d-flex align-items-center hover-li text-light nav-link rounded-2 my-2">
-                <li className="py-2 px-3">
-                  <FaUserCog className="me-2 fs-5"/>
-                  Teachers
-                </li>
-              </NavLink>
-              <a href="" className="d-flex align-items-center hover-li text-light nav-link rounded-2 my-2">
-                <li className="py-2 px-3">
-                  <RiHomeSmile2Fill className="me-2 fs-5"/>
-                  Classes
-                </li>
-              </a>
-              <a href="" className="d-flex align-items-center hover-li text-light nav-link rounded-2 my-2">
-                <li className="py-2 px-3">
-                  <MdDateRange className="me-2 fs-5"/>
-                  Attendence
-                </li>
-              </a>
-              <NavLink to="course" className="d-flex align-items-center hover-li text-light nav-link rounded-2 my-2">
-                <li className="py-2 px-3">
-                  <FaBookOpen className="me-2 fs-5"/>
-                  Courses
-                </li>
-              </NavLink>
+            <NavLink to="/" className="d-flex align-items-center hover-li text-light nav-link rounded-2 my-2">
+              <li className="py-2 px-3">
+                <FaRegWindowRestore className="me-2 fs-5" />
+                Dashboard
+              </li>
+            </NavLink>
+            <NavLink to="/student" className="d-flex align-items-center hover-li text-light nav-link rounded-2 my-2">
+              <li className="py-2 px-3">
+                <FaUserFriends className="me-2 fs-5" />
+                Students
+              </li>
+            </NavLink>
+            <NavLink to="/teacher" className="d-flex align-items-center hover-li text-light nav-link rounded-2 my-2">
+              <li className="py-2 px-3">
+                <FaUserCog className="me-2 fs-5" />
+                Teachers
+              </li>
+            </NavLink>
+            <a href="#" className="d-flex align-items-center hover-li text-light nav-link rounded-2 my-2">
+              <li className="py-2 px-3">
+                <RiHomeSmile2Fill className="me-2 fs-5" />
+                Classes
+              </li>
+            </a>
+            <a href="#" className="d-flex align-items-center hover-li text-light nav-link rounded-2 my-2">
+              <li className="py-2 px-3">
+                <MdDateRange className="me-2 fs-5" />
+                Attendance
+              </li>
+            </a>
+            <NavLink to="course" className="d-flex align-items-center hover-li text-light nav-link rounded-2 my-2">
+              <li className="py-2 px-3">
+                <FaBookOpen className="me-2 fs-5" />
+                Courses
+              </li>
+            </NavLink>
           </ul>
         </menu>
+
+        {/* Footer - Log Out Button */}
+        <footer>
+          <NavLink to="/login" className="btn btn-danger w-100 d-flex align-items-center justify-content-center">
+            <IoLogOut className="fs-3 me-2" />
+            Log out
+          </NavLink>
+        </footer>
       </div>
     </aside>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import 'animate.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import "animate.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/js/bootstrap.js'
-import Dashboard from './pages/Dashboard.jsx'
-import NotFound from './pages/NotFound.jsx'
-import Teacher from './pages/Teacher.jsx'
-import Student from './pages/Student.jsx'
-import Course from './pages/Course.jsx'
-import Login from './pages/Login.jsx'
-import Register from './pages/Register.jsx'
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import Dashboard from "./pages/Dashboard.jsx";
+import NotFound from "./pages/NotFound.jsx";
+import Teacher from "./pages/Teacher.jsx";
+import Student from "./pages/Student.jsx";
+import Course from "./pages/Course.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+import Classes from "./pages/Classes.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<App/>}>
-          <Route index element={<Dashboard />} />  {/* Default child route */}
-          <Route path='teacher' element={<Teacher />} />
-          <Route path='student' element={<Student />} />
-          <Route path='course' element={<Course />} />        
+        <Route path="/" element={<App />}>
+          <Route index element={<Dashboard />} /> {/* Default child route */}
+          <Route path="teacher" element={<Teacher />} />
+          <Route path="student" element={<Student />} />
+          <Route path="course" element={<Course />} />
+          <Route path="class" element={<Classes />} />
         </Route>
-          <Route path='*' element={<NotFound />} /> {/* Catch-all inside App */}
-          
-          <Route path='login' element={<Login/>}/>
-          <Route path='register' element={<Register/>} />
-     
+        <Route path="*" element={<NotFound />} /> {/* Catch-all inside App */}
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
       </Routes>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
